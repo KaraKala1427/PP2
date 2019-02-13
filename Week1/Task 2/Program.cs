605fe5097a0c109e2d2c42f7b513ad62a2ebb612
KaraKala1427 <ernarerbol027@gmail.com>
2019-02-14 02:55:09 +0600

    task2 solved

--- Week1/Task 2/Program.cs
+++ Week1/Task 2/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace Task_2
 {
+    class Student //creating class
+    {
+        public string name;
+        public string id;            //variables of the class
+        public int yearofstudy;
+
+        public Student(string name, string id)    //constructor with two parametr
+        {
+            this.name = name;
+            this.id = id;
+            PrintInfo();   //calling the method
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Name of the student: "+name);  //method that outputs the data of student
+            Console.WriteLine("Id of the student: "+id);
+        }
+        public int Incremented(int a)    //method that increment year of student
+        {
+            yearofstudy = a;
+            return yearofstudy + 1;
+        }
+    }
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your id: ");        //inputting the data of student
+            string id = Console.ReadLine();
+            Console.Write("Enter year of study: ");
+            string year = Console.ReadLine();
+            int number = int.Parse(year);
+            Student student = new Student(name,id);   //constructor
+            Console.WriteLine("incremented year of the student: "+student.Incremented(number));
+            
+
+
+          
         }
     }
 }
