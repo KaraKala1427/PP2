de7f1e2b86780e58844743adf2ffbabdbafad11f
KaraKala1427 <ernarerbol027@gmail.com>
2019-02-15 00:36:03 +0600

    solutions for task3 and task4

---
+++ Week2/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream f = new FileStream(@"C:\test\test22\copy.txt", FileMode.Create)) { }   //created file
+            File.Copy(@"C:\test\test22\copy.txt", @"C:\test\test22\get\copy.txt", true);   //copying it to another directory
+            File.Delete(@"C:\test\test22\copy.txt");   //deleting it from his directory
+
+        }
+    }
+}
